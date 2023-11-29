4e8a1f8fb01e3f1211bae5fb976df57a0ab72a3f
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-29 14:29:59 -0300

    refactor: move `off` to ts file

--- src/lib/typescript.ts
+++ src/lib/typescript.ts
@@ -18,7 +18,10 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
 
   const rules: Rules = {
     '@typescript-eslint/adjacent-overload-signatures': 'error',
+
+    'no-array-constructor': 'off',
     '@typescript-eslint/no-array-constructor': 'error',
+
     '@typescript-eslint/array-type': ['error', { default: 'array' }],
     '@typescript-eslint/ban-ts-comment': [
       'error',
@@ -31,6 +34,8 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
       }
     ],
     '@typescript-eslint/class-literal-property-style': ['error', 'fields'],
+
+    'class-methods-use-this': 'off',
     '@typescript-eslint/class-methods-use-this': [
       'error',
       {
@@ -38,6 +43,7 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
         ignoreClassesThatImplementAnInterface: 'public-fields'
       }
     ],
+
     '@typescript-eslint/consistent-generic-constructors': ['error', 'constructor'],
     '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
     '@typescript-eslint/consistent-type-assertions': [
@@ -45,7 +51,10 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
       { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' }
     ],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+
+    'dot-notation': 'off',
     '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
+
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': [
       'error',
@@ -74,14 +83,20 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
     '@typescript-eslint/no-duplicate-enum-values': 'error',
     '@typescript-eslint/no-duplicate-type-constituents': 'error',
     '@typescript-eslint/no-dynamic-delete': 'error',
+
+    'no-empty-function': 'off',
     '@typescript-eslint/no-empty-function': 'error',
+
     '@typescript-eslint/no-empty-interface': 'error',
     '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true }],
     '@typescript-eslint/no-extra-non-null-assertion': 'error',
     '@typescript-eslint/no-extraneous-class': 'error',
     '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
     '@typescript-eslint/no-for-in-array': 'error',
+
+    'no-implied-eval': 'off',
     '@typescript-eslint/no-implied-eval': 'error',
+
     '@typescript-eslint/no-import-type-side-effects': 'error',
     '@typescript-eslint/no-inferrable-types': 'error',
     '@typescript-eslint/no-invalid-void-type': [
@@ -91,8 +106,13 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
         allowAsThisParameter: false
       }
     ],
+
+    'no-loop-func': 'off',
     '@typescript-eslint/no-loop-func': 'error',
+
+    'no-loss-of-precision': 'off',
     '@typescript-eslint/no-loss-of-precision': 'error',
+
     '@typescript-eslint/no-meaningless-void-operator': 'error',
     '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-misused-promises': [
@@ -116,9 +136,15 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
     '@typescript-eslint/no-non-null-assertion': 'error',
     '@typescript-eslint/no-redundant-type-constituents': 'error',
     '@typescript-eslint/no-require-imports': 'error',
+
+    'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
+
     '@typescript-eslint/no-this-alias': 'error',
+
+    'no-throw-literal': 'off',
     '@typescript-eslint/no-throw-literal': 'error',
+
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': [
       'error',
       {
@@ -139,7 +165,11 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
     '@typescript-eslint/no-unsafe-member-access': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
     '@typescript-eslint/no-unsafe-unary-minus': 'error',
+
+    'no-unused-expressions': 'off',
     '@typescript-eslint/no-unused-expressions': 'error',
+
+    'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': [
       'error',
       {
@@ -148,7 +178,10 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
         typedefs: false
       }
     ],
+
+    'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
+
     '@typescript-eslint/no-useless-empty-export': 'error',
     '@typescript-eslint/non-nullable-type-assertion-style': 'error',
     '@typescript-eslint/prefer-as-const': 'error',
@@ -181,7 +214,10 @@ export async function typescript(options: ConfigOptions): Promise<ConfigObject> 
     '@typescript-eslint/prefer-ts-expect-error': 'error',
     '@typescript-eslint/promise-function-async': 'error',
     '@typescript-eslint/require-array-sort-compare': 'error',
+
+    'require-await': 'off',
     '@typescript-eslint/require-await': 'error',
+
     '@typescript-eslint/restrict-plus-operands': 'error',
     '@typescript-eslint/restrict-template-expressions': 'error',
     '@typescript-eslint/strict-boolean-expressions': 'off',
