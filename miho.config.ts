96c4e8aa91a77cf56b65fa9976932c7404ace894
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-29 15:58:39 -0300

    chore: change to ts

---
+++ miho.config.ts
@@ -0,0 +1,34 @@
+import { Octokit } from '@octokit/core';
+import { PackageManager, defineConfig } from 'miho';
+
+export default defineConfig({
+  packageManager: PackageManager.PNPM,
+  recursive: false,
+  verbose: true,
+  commit: {
+    push: true,
+    all: true
+  },
+  jobs: {
+    build: true,
+    publish: async () => {
+      const { version } = await import('./package.json');
+      const { GITHUB_TOKEN } = await import('./config.json');
+      const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+      await octokit.request('POST /repos/{owner}/{repo}/releases', {
+        tag_name: version,
+        name: version,
+        draft: false,
+        prerelease: false,
+        generate_release_notes: true,
+        owner: 'ferreira-tb',
+        repo: 'eslint-config',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+          accept: 'application/vnd.github+json'
+        }
+      });
+    }
+  }
+});
