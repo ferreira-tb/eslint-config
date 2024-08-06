95f47e01fa35d73fc651717999fb55c0e49e46f9
Andrew Ferreira <andrew.shien2@gmail.com>
2024-08-06 16:13:32 -0300

    chore: bump version

--- src/lib/vue.ts
+++ src/lib/vue.ts
@@ -39,7 +39,7 @@ export async function vue(options: ConfigOptions): Promise<Partial<ConfigObject>
     }],
     'vue/component-api-style': ['error', ['script-setup']],
     'vue/component-definition-name-casing': ['error', 'PascalCase'],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case', {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase', {
       registeredComponentsOnly: false,
     }],
     'vue/custom-event-name-casing': ['error', 'kebab-case'],
