6b87ded4f3e4a4628b8323942cb3570292d505b5
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-05 12:20:21 -0300

    fix: fix plugin import

--- src/lib/unocss.ts
+++ src/lib/unocss.ts
@@ -4,24 +4,25 @@ import { interopDefault, isEnabled, mapRules } from '../utils';
 export async function unocss(options: ConfigOptions): Promise<Partial<ConfigObject>> {
   if (!isEnabled(options.features, 'unocss')) return {};
 
-  const plugin = await interopDefault(import('@unocss/eslint-config/flat'));
+  const flatConfig = await interopDefault(import('@unocss/eslint-config/flat'));
+  const plugin = (flatConfig as any).plugins?.unocss;
 
   const overrides = mapRules(options.overrides?.unocss ?? {}, (rule, value) => {
-    if (rule.startsWith('@unocss/')) {
+    if (rule.startsWith('unocss/')) {
       return [rule, value];
     }
     else {
-      return [`@unocss/${rule}`, value];
+      return [`unocss/${rule}`, value];
     }
   });
 
   return {
-    plugins: { '@unocss': plugin },
+    plugins: { unocss: plugin },
     rules: {
-      '@unocss/blocklist': 'off',
-      '@unocss/enforce-class-compile': 'off',
-      '@unocss/order': 'error',
-      '@unocss/order-attributify': 'off',
+      'unocss/blocklist': 'off',
+      'unocss/enforce-class-compile': 'off',
+      'unocss/order': 'error',
+      'unocss/order-attributify': 'off',
       ...overrides,
     },
   };
